939bcbaa9a46f1257e725adcab7d12909ac0a0c2
Caio Amaral <caiobr98@gmail.com>
2025-11-23 20:03:39 -0300

    Added getters for users

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -18,4 +18,27 @@ describe('User Entity', () => {
         expect(sut.props.password).toEqual(props.password);
         expect(sut.props.createdAt).toBeInstanceOf(Date);
     });
+
+    it('Get name method', () => {
+        expect(sut.props.name).toBeDefined();
+        expect(sut.props.name).toEqual(props.name);
+        expect(typeof sut.props.name).toBe('string');
+    });
+
+    it('Get email method', () => {
+        expect(sut.props.email).toBeDefined();
+        expect(sut.props.email).toEqual(props.email);
+        expect(typeof sut.props.email).toBe('string');
+    });
+
+    it('Get password method', () => {
+        expect(sut.props.password).toBeDefined();
+        expect(sut.props.password).toEqual(props.password);
+        expect(typeof sut.props.password).toBe('string');
+    });
+
+    it('Get createdAt method', () => {
+        expect(sut.props.createdAt).toBeDefined();
+        expect(sut.props.createdAt).toBeInstanceOf(Date);
+    });
 });
